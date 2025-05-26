ea44f6df940c610027ceed71b9fb71cf9969721f
K3yyy <yunmenghuy33@gmail.com>
2025-05-26 17:05:02 +0700

    feat(testcase):testcase crud operation

--- src/main/java/com/both/testing_pilot_backend/service/TestcaseService.java
+++ src/main/java/com/both/testing_pilot_backend/service/TestcaseService.java
@@ -1,9 +1,16 @@
 package com.both.testing_pilot_backend.service;
 
+import com.both.testing_pilot_backend.dto.request.PageRequest;
+import com.both.testing_pilot_backend.dto.request.ProjectRequest;
+import com.both.testing_pilot_backend.model.Project;
 import com.both.testing_pilot_backend.model.TestCase;
+import org.springframework.util.MultiValueMap;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface TestcaseService {
-    public TestCase getTestCaseById(UUID id);
+     TestCase getTestCaseById(UUID id);
+
+
 }
