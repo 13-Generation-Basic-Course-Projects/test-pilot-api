2496145c85aca239908a93c61a33d09eff3f5be3
yumateb <yumateb@gmail.com>
2025-05-30 22:15:10 +0700

    fix(auth): rollback the missing of email service

--- src/main/java/com/both/testing_pilot_backend/service/EmailService.java
+++ src/main/java/com/both/testing_pilot_backend/service/EmailService.java
@@ -1,10 +1,12 @@
 package com.both.testing_pilot_backend.service;
 
-import com.both.testing_pilot_backend.event.UserRegistrationEvent;
 import com.both.testing_pilot_backend.event.ForgetPasswordEvent;
+import com.both.testing_pilot_backend.event.UserRegistrationEvent;
+
+import java.util.Map;
 
 public interface EmailService {
-    void sendHtmlEmail(String to, String subject, String htmlBody);
-    void sendRegistrationVerification(UserRegistrationEvent event);
-    void sendForgetPasswordRequest(ForgetPasswordEvent event);
+    void sendRegistrationVerification(UserRegistrationEvent payload);
+    void sendForgetPasswordRequest(ForgetPasswordEvent payload);
+    void sendTemplatedEmail(String to, String subjectKey, String templateName, Map<String, Object> variables);
 }
