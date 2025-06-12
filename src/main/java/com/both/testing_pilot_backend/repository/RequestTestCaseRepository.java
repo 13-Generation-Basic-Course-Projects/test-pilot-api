58045fa4777bbba012f692be5418f6233dd7885b
Chompou <1220.duong.angchompou@rupp.edu.kh>
2025-06-12 15:12:38 +0700

    chore(Refactoring) Refactoring requeset test cases repository

--- src/main/java/com/both/testing_pilot_backend/repository/RequestTestCaseRepository.java
+++ src/main/java/com/both/testing_pilot_backend/repository/RequestTestCaseRepository.java
@@ -1,4 +1,3 @@
-// src/main/java/com/both/testing_pilot_backend/repository/RequestTestCaseRepository.java
 package com.both.testing_pilot_backend.repository;
 
 import com.both.testing_pilot_backend.model.RequestTestCase;
@@ -12,54 +11,54 @@ import java.util.UUID;
 public interface RequestTestCaseRepository {
 
     @Results(id = "requestTestCaseMapper", value = {
-            @Result(property = "id", column = "rtc.id"),
-            @Result(property = "requestId", column = "rtc.request_id"),
-            @Result(property = "testCaseId", column = "rtc.test_case_id"),
-            @Result(property = "applicationContext", column = "rtc.application_context"),
-            @Result(property = "isExpectedSuccess", column = "rtc.is_expected_success"),
-            @Result(property = "createdAt", column = "rtc.created_at"),
-            @Result(property = "updatedAt", column = "rtc.updated_at"),
-            @Result(property = "request", column = "rtc.request_id", one = @One(select = "com.both.testing_pilot_backend.repository.RequestRepository.findById")),
-            @Result(property = "testCase", column = "rtc.test_case_id", one = @One(select = "com.both.testing_pilot_backend.repository.TestCaseRepository.findById"))
+            @Result(property = "id", column = "id"),
+            @Result(property = "requestId", column = "request_id"),
+            @Result(property = "testCaseId", column = "test_case_id"),
+            @Result(property = "applicationContext", column = "application_context"),
+            @Result(property = "isExpectedSuccess", column = "is_expected_success"),
+            @Result(property = "createdAt", column = "created_at"),
+            @Result(property = "updatedAt", column = "updated_at"),
+            @Result(property = "request", column = "request_id", one = @One(select = "com.both.testing_pilot_backend.repository.RequestRepository.findById")),
+            @Result(property = "testCase", column = "test_case_id", one = @One(select = "com.both.testing_pilot_backend.repository.TestCaseRepository.findById"))
     })
     @Select("""
             INSERT INTO request_test_cases (request_id, test_case_id, application_context, is_expected_success, created_at, updated_at)
-            VALUES (#{rtc.requestId}, #{rtc.testCaseId}, #{rtc.applicationContext}::application_context_type_enum, #{rtc.isExpectedSuccess}, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)
+            VALUES (#{req.requestId}, #{req.testCaseId}, #{req.applicationContext}::application_context_type_enum, #{req.isExpectedSuccess}, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)
             RETURNING *;
             """)
-    RequestTestCase save(@Param("rtc") RequestTestCase requestTestCase);
+    RequestTestCase save(@Param("req") RequestTestCase requestTestCase);
 
     @ResultMap("requestTestCaseMapper")
     @Select("""
-            SELECT rtc.*
-            FROM request_test_cases rtc
-            WHERE rtc.id = #{id};
+            SELECT *
+            FROM request_test_cases
+            WHERE id = #{id};
             """)
     RequestTestCase findById(@Param("id") UUID id);
 
     @ResultMap("requestTestCaseMapper")
     @Select("""
-            SELECT rtc.*
-            FROM request_test_cases rtc
-            WHERE rtc.request_id = #{requestId};
+            SELECT *
+            FROM request_test_cases
+            WHERE request_id = #{requestId};
             """)
     List<RequestTestCase> findByRequestId(@Param("requestId") UUID requestId);
 
     @ResultMap("requestTestCaseMapper")
     @Select("""
-            SELECT rtc.*
-            FROM request_test_cases rtc
-            WHERE rtc.test_case_id = #{testCaseId};
+            SELECT *
+            FROM request_test_cases
+            WHERE test_case_id = #{testCaseId};
             """)
     List<RequestTestCase> findByTestCaseId(@Param("testCaseId") UUID testCaseId);
 
     @ResultMap("requestTestCaseMapper")
     @Select("""
-        SELECT * FROM request_test_cases rtc
-        WHERE rtc.test_case_id = #{testCaseId}
-        AND rtc.request_id = #{requestId}
-        AND rtc.application_context = #{applicationContext}::application_context_type_enum
-        AND rtc.target_field_path = #{targetFieldPath}
+        SELECT * FROM request_test_cases
+        WHERE test_case_id = #{testCaseId}
+        AND request_id = #{requestId}
+        AND application_context = #{applicationContext}::application_context_type_enum
+        AND target_field_path = #{targetFieldPath}
          LIMIT 1;
     """)
     RequestTestCase findByRequestIdAndTestCaseIdAndApplicationContextAndTargetFieldPath (
@@ -71,10 +70,10 @@ public interface RequestTestCaseRepository {
 
     @ResultMap("requestTestCaseMapper")
     @Select("""
-            SELECT rtc.*
-            FROM request_test_cases rtc
-            WHERE rtc.request_id = #{requestId}
-            AND rtc.test_case_id = #{testCaseId}
+            SELECT *
+            FROM request_test_cases
+            WHERE request_id = #{requestId}
+            AND test_case_id = #{testCaseId}
             LIMIT 1;
             """)
     RequestTestCase findByRequestIdAndTestCaseId(@Param("requestId") UUID requestId, @Param("testCaseId") UUID testCaseId);
